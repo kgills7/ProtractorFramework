fe0afc7ab6aac75b649c099e8c6f4374ac927da6
Kelly Gills <kgills@corp.membersuite.com>
2019-11-14 13:07:29 -0500

    my first project commit

---
+++ tests/calculator.js
@@ -0,0 +1,47 @@
+let homepage = require('../pages/homepage')
+
+describe('demo calculator tests', function(){
+
+it('addition test', function(){
+
+    //browser.get('http://juliemr.github.io/protractor-demo/');
+    homepage.get('http://juliemr.github.io/protractor-demo/');
+  
+
+    //element(by.model('first')).sendKeys('2');
+    homepage.enterFirstNumber('4');
+
+    //element(by.model('second')).sendKeys('3');
+    homepage.enterSecondNumber('3');
+
+    //element (by.css('[ng-click="doAddition()"]')).click(4);
+    homepage.clickGo();
+
+
+    //let result = element(by.cssContainingText('.ng-binding', '5'));
+    //expect(result.getText()).toEqual('5');
+    homepage.verifyResult('7');
+  
+
+    browser.sleep(5000)
+
+});
+
+it('subtraction test', function(){
+
+    homepage.get('http://juliemr.github.io/protractor-demo/');
+  
+    homepage.enterFirstNumber('4');
+
+    homepage.enterSecondNumber('3');
+
+    homepage.clickGo1();
+
+    homepage.verifyResult('7');
+  
+
+    browser.sleep(5000)
+
+});
+
+});
