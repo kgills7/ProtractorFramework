fb36b74053311c5d8352ad3d9946da2835b5c6fa
Kelly Gills <kgills@membersuite.com>
2019-11-18 10:46:57 -0500

    KG test

--- conf/conf.js
+++ conf/conf.js
@@ -1,11 +1,4 @@
 // An example configuration file.
-var HtmlScreenshotReporter = require('protractor-jasmine2-screenshot-reporter');
-
-var reporter = new HtmlScreenshotReporter({
-  dest: 'target/screenshots',
-  filename: 'my-report.html'
-});
-
 exports.config = {
   directConnect: true,
 
@@ -24,30 +17,7 @@ exports.config = {
   // Options to be passed to Jasmine.
   jasmineNodeOpts: {
     defaultTimeoutInterval: 30000
-  },
-
-  // Setup the report before any tests start
-  beforeLaunch: function () {
-    return new Promise(function (resolve) {
-      reporter.beforeLaunch(resolve);
-    });
-  },
-
-  // Assign the test reporter to each running instance
-  onPrepare: function () {
-
-    jasmine.getEnv().addReporter(reporter);
-    var AllureReporter = require('jasmine-allure-reporter');
-    jasmine.getEnv().addReporter(new AllureReporter({
-      resultsDir: 'allure-results'
-    }));
-  },
-
-  // Close the report after all tests finish
-  afterLaunch: function (exitCode) {
-    return new Promise(function (resolve) {
-      reporter.afterLaunch(resolve.bind(this, exitCode));
-    });
   }
+
 
 };
