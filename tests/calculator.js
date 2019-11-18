f48b8f9a2bb6485c288e3546ad1fce8091824a6a
Kelly Gills <kgills@membersuite.com>
2019-11-18 11:39:53 -0500

    Calc update

--- tests/calculator.js
+++ tests/calculator.js
@@ -27,21 +27,4 @@ it('addition test', function(){
 
 });
 
-it('subtraction test', function(){
-
-    homepage.get('http://juliemr.github.io/protractor-demo/');
-  
-    homepage.enterFirstNumber('4');
-
-    homepage.enterSecondNumber('3');
-
-    homepage.clickGo1();
-
-    homepage.verifyResult('7');
-  
-
-    browser.sleep(5000)
-
-});
-
 });
